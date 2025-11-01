87021e6a365662452d4017e406cb8cfa8184efcd
GSNFonseka <nowanyafonseka@gmail.com>
2025-11-01 23:31:41 +0530

    feat: add service class

---
+++ demoBrowser.py
@@ -0,0 +1,31 @@
+import time
+
+from selenium import webdriver
+#Chrome driver service Selenium 160->160 chrome driver
+driver = webdriver.Chrome()
+driver.get("https://www.github.com")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+time.sleep(2)
