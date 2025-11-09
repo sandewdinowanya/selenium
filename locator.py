b7b708cb8e0d69fe8618b33301915321b1848ecc
GSNFonseka <nowanyafonseka@gmail.com>
2025-11-09 23:28:32 +0530

    feat: add methods to identify element attributes for automated testing

--- locator.py
+++ locator.py
@@ -13,6 +13,20 @@ driver.find_element(By.NAME,"email").send_keys("helloworld@gmail.com")
 driver.find_element(By.ID,"exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID,"exampleCheck1").click()
 
+#Xpath - //tagname[@attribute='value']
+       # //input[@type='submit']
+
+#CSS Selector - tagname[@attribute='value']
+            #input[@type='submit']
+            # id
+            # .classname
 
 
-time.sleep(500 )
+driver.find_element(By.CSS_SELECTOR, "inlineRadio1").click()
+driver.find_element(By.XPATH,"//input[@type='submit']").click()
+message = driver.find_element(By.CLASS_NAME,"alert-success").text
+print(message) 
+
+assert 'success' in message
+
+time.sleep(500)
