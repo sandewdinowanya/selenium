d00e3e51162740cb36b73c1590653ad601af6114
GSNFonseka <nowanyafonseka@gmail.com>
2025-11-24 11:35:52 +0530

    feat: create a loop for printing the values on a table

--- excelDemo.py
+++ excelDemo.py
@@ -1,14 +1,20 @@
 import openpyxl
-book = openpyxl.load_workbook("C:\\Users\\Hp\\Desktop\\PythonDemo.xlsx")
+book = openpyxl.load_workbook("C:\\Users\\Hp\\PycharmProjects\\PythonTesting\\selenium\\Book1.xlsx")
 sheet = book.active
 cell = sheet.cell(row=1,column=2)
-print(cell.value)
+# print(cell.value)
 sheet.cell(row=2, column=2).value = "Rahul"
 
-print(sheet.cell(row=2, column=2).value)
+# print(sheet.cell(row=2, column=2).value)
+#
+# print(sheet.max_row)
+#
+# print(sheet.max_column)
+#
+# print(sheet['a5'].value)
 
-print(sheet.max_row)
-
-print(sheet.max_column)
+for r in range(1,sheet.max_row+1):
+    for c in range(1,sheet.max_column+1):
+        print(sheet.cell(row=r,column=c).value)
 
 
