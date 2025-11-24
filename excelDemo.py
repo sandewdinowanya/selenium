02628dcddee7ab5a8605c712a668ae2c84885fac
GSNFonseka <nowanyafonseka@gmail.com>
2025-11-24 13:00:54 +0530

    feat: create a dictionary with storing values in Excel table

--- excelDemo.py
+++ excelDemo.py
@@ -1,6 +1,9 @@
+from typing import Dict
+
 import openpyxl
 book = openpyxl.load_workbook("C:\\Users\\Hp\\PycharmProjects\\PythonTesting\\selenium\\Book1.xlsx")
 sheet = book.active
+Dict = {}
 cell = sheet.cell(row=1,column=2)
 # print(cell.value)
 sheet.cell(row=2, column=2).value = "Rahul"
@@ -14,7 +17,8 @@ sheet.cell(row=2, column=2).value = "Rahul"
 # print(sheet['a5'].value)
 
 for r in range(1,sheet.max_row+1):
-    for c in range(1,sheet.max_column+1):
-        print(sheet.cell(row=r,column=c).value)
+    if sheet.cell(row=r, column=1).value == "TestCase2":
+        for c in range(1,sheet.max_column+1):
+            Dict[sheet.cell(row=1, column=c).value] = sheet.cell(row=r, column=c).value
 
-
+print(Dict)
