4e30fa1f05f8197fc9ac8e001d3d10a4b47a21cd
GSNFonseka <nowanyafonseka@gmail.com>
2025-12-04 23:26:55 +0530

    Uncomment print statements for debugging
    Previously commented-out print statements have been uncommented to display cell values, max row, max column, and a specific cell value. This aids in debugging and inspecting the Excel sheet's contents during execution.

--- excelDemo.py
+++ excelDemo.py
@@ -8,13 +8,13 @@ cell = sheet.cell(row=1,column=2)
 # print(cell.value)
 sheet.cell(row=2, column=2).value = "Rahul"
 
-# print(sheet.cell(row=2, column=2).value)
-#
-# print(sheet.max_row)
-#
-# print(sheet.max_column)
-#
-# print(sheet['a5'].value)
+print(sheet.cell(row=2, column=2).value)
+
+print(sheet.max_row)
+
+print(sheet.max_column)
+
+print(sheet['a5'].value)
 
 for r in range(1,sheet.max_row+1):
     if sheet.cell(row=r, column=1).value == "TestCase2":
