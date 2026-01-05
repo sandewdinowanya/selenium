a673dba4867fb2adfe064ed8b0e324e582f0956d
GSNFonseka <nowanyafonseka@gmail.com>
2026-01-05 23:56:57 +0530

    Add product selection and checkout to test script
    feat: create end-to-end test to navigate to the shop page, select the 'Blackberry' product, add it to the cart, and proceed to checkout

--- endtoendtest.py
+++ endtoendtest.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 #chrome driver
 driver = webdriver.Chrome()
 
-driver.get("http://google.com")
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+
+driver.implicitly_wait(4)
+
+# //a[contains(@href,'shop')]  css selector -> a[href*='shop']
+driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
+
+products =driver.find_elements(By.XPATH,"//div[@class='card h-100']")
+
+for product in products:
+    product_name=product.find_element(By.XPATH,"div/h4/a").text
+    if product_name =='Blackberry':
+        product.find_element(By.XPATH,"div/button").click()
+
+driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
+print("Success")
