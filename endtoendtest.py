2f48e85790a6bc6f3a5d5bb2951eb7e1fc2ed8b0
GSNFonseka <nowanyafonseka@gmail.com>
2026-01-08 23:57:04 +0530

    feat: add comments and improve end-to-end test flow
    Added descriptive comments to clarify test steps, included explicit waits and auto-suggestion handling, and improved form submission and assertion logic. The driver is now properly closed at the end of the test.

--- endtoendtest.py
+++ endtoendtest.py
@@ -8,6 +8,7 @@ driver = webdriver.Chrome()
 
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 
+#implicit wait
 driver.implicitly_wait(4)
 
 # //a[contains(@href,'shop')]  css selector -> a[href*='shop']
@@ -15,6 +16,7 @@ driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
 
 products =driver.find_elements(By.XPATH,"//div[@class='card h-100']")
 
+#Product chaining
 for product in products:
     product_name=product.find_element(By.XPATH,"div/h4/a").text
     if product_name =='Blackberry':
@@ -22,10 +24,26 @@ for product in products:
 
 driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
 driver.find_element(By.XPATH," //button[@class='btn btn-success']").click()
-driver.find_element(By.ID,"country").send_keys("ind")
-wait = WebDriverWait(driver,10)
-wait.until(expected_conditions.presence_of_element_located(By.LINK_TEXT, "India"))
-driver.find_element(By.LINK_TEXT, "India").click()
 
+#searching
+driver.find_element(By.ID,"country").send_keys("ind")
+
+#explicit wait
+wait = WebDriverWait(driver,10)
+
+#auto suggestion
+wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "India")))
+driver.find_element(By.LINK_TEXT, "India").click()
+driver.find_element(By.XPATH,"//div[@class='checkbox checkbox-primary']").click()
+
+#optional tag names - "[type='submit']"
+driver.find_element(By.CSS_SELECTOR,"[type='submit']").click()
+successText = driver.find_element(By.CLASS_NAME,"alert-success").text
+
+#regular expression with asert
+assert "Success! Thank you!" in successText
 
 print("Success")
+
+#close the driver 
+driver.close()
